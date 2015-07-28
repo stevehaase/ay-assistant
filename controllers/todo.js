f3e456febd73cdc1ca9bf61b87b655826b5dbd86
Steve Haase <stevehaase1@gmail.com>
2015-07-28 08:22:11 -0400

    Trying things out to get the completedTodos on the page

--- controllers/todo.js
+++ controllers/todo.js
@@ -3,13 +3,14 @@ var User = require('../models/User');
 exports.postTodo = function(req, res, next){
 	User.findById(req.user.id, function(err, user) {
 		if (err) return next(err);
-		user.todo.unshift(req.body.todo);
-		console.log(user);
-		user.save(function(err){
-			if (err) return next(err);
-			res.redirect('/');
-			res.render('home.jade', {todos: user.todo});
-		})
+		if (req.body.todo){
+			user.todo.unshift(req.body.todo);	
+			user.save(function(err){
+				if (err) return next(err);
+				res.render('home.jade', {todos: user.todo});
+			})
+		}
+		
 	});
 };
 
@@ -18,12 +19,16 @@ exports.completeTodo = function(req, res, next){
 		if (err) return next(err)
 		var num = req.body.item;
 		var thingDone = user.todo[num];
-		user.completedTodo.unshift(thingDone);
-		user.todo.splice(num, 1);
-		user.save(function(err){
-			if (err) return next(err);
-			res.render('home.jade', {todos: user.todo});
-		})
+		if (thingDone){
+			user.completedTodo.unshift(thingDone);
+			user.todo.splice(num, 1);	
+			user.save(function(err){
+				if (err) return next(err);
+				res.json({todos: user.todo, completedTodos: user.completedTodo});
+				res.redirect('/');
+			})
+		}
+		
 		
 	})
 
