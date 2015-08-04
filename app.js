3ee390c09b43c9a72cc9befea319199eae8675f3
Steve Haase <stevehaase1@gmail.com>
2015-08-03 21:36:58 -0400

    dangerous things with passowrds

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.locals.moment = require('moment');
 /**
  * Connect to MongoDB.
  */
-mongoose.connect(secrets.db);
+mongoose.connect('mongodb://stevehaase:Lightchooser1@ds029793.mongolab.com:29793/heroku_7qkgm9vt');
 mongoose.connection.on('error', function() {
   console.error('MongoDB Connection Error. Please make sure that MongoDB is running.');
 });
