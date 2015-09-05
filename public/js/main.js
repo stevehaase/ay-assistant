b5ddcdc099bf6042f346b459d8c1a3072b6c1dbd
Steve Haase <stevehaase1@gmail.com>
2015-09-05 09:27:44 -0400

    Note editor now only shows when you click the pencil

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,25 @@
 $(document).ready(function() {
 
-        var completeTodo = function(num){
+				var completeTodo = function(num){
 
-          var done = document.getElementsByName('completed')[num];
-          var spinner = done.nextSibling;
-          console.log(spinner);
-          spinner.innerHTML = "<i class='fa fa-spinner fa-pulse'></i>";
-          
-          
-          $.ajax({
-            type: 'POST',
-            beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
-            url: '/todoComplete',
-            data: {item: num},
-            success: function() {
-              window.location.reload(true);
-            }
-          });
-      
-       	}
+					var done = document.getElementsByName('completed')[num];
+					var spinner = done.nextSibling;
+					console.log(spinner);
+					spinner.innerHTML = "<i class='fa fa-spinner fa-pulse'></i>";
+					
+					
+					$.ajax({
+						type: 'POST',
+						beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
+						url: '/todoComplete',
+						data: {item: num},
+						success: function() {
+							window.location.reload(true);
+						}
+					});
+			
+				}
+
+				$('#notesfrm').hide()
 
 });
